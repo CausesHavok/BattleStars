2a22869d934e5a3262b8a4ba3018454939b847b6
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-08-03 15:50:55 +0200

    Basic working project

---
+++ BattleStars/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "BattleStars - Square Test");
+Raylib.SetTargetFPS(60);
+
+int x = 100;
+int y = 100;
+
+while (!Raylib.WindowShouldClose())
+{
+    // Movement
+    if (Raylib.IsKeyDown(KeyboardKey.Right)) x += 5;
+    if (Raylib.IsKeyDown(KeyboardKey.Left)) x -= 5;
+    if (Raylib.IsKeyDown(KeyboardKey.Up)) y -= 5;
+    if (Raylib.IsKeyDown(KeyboardKey.Down)) y += 5;
+
+    // Draw
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+    Raylib.DrawText("Arrow keys move the square", 10, 10, 20, Color.White);
+    Raylib.DrawRectangle(x, y, 50, 50, Color.Red);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
