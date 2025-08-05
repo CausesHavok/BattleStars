613196c5340133717ef8251fab120893a93fa3c6
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-08-05 10:40:11 +0200

    Shot skeleton class for modelling shots fired by enemies and player

---
+++ BattleStars/Shot.cs
@@ -0,0 +1,19 @@
+using System.Numerics;
+
+public class Shot
+{
+    public Vector2 Position { get; set; }
+    public Vector2 Direction { get; set; }
+    public float Speed { get; set; }
+    public float Damage { get; set; }
+
+    public Shot(Vector2 position, Vector2 direction, float speed, float damage)
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+}
