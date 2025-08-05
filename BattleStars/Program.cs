dbe523fcbcac2e6686baf3696daa0b3a75aa77e3
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-08-05 16:53:10 +0200

    Also added shots to the "game"

--- BattleStars/Program.cs
+++ BattleStars/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 using BattleStars;
-using System.Collections.Generic;
 
 Raylib.InitWindow(800, 600, "BattleStars - Square Test");
 Raylib.SetTargetFPS(60);
@@ -10,25 +9,33 @@ Raylib.SetTargetFPS(60);
 var boundaryChecker = new BoundaryChecker(0, 800 - 50, 0, 600 - 50); // 50 is the player size
 
 // Create player at starting position
-var player = new Player(new Vector2(100, 100), 100f, boundaryChecker);
+var player = new Player(new Vector2(100, 100), 100f, boundaryChecker, ShotFactory.CreateLaserShot); // Initial shot parameters
 
 // Create enemies at different positions
 var enemies = new List<Enemy>
 {
-    new Enemy(new Vector2(400, 100), 50f),
-    new Enemy(new Vector2(200, 300), 50f),
-    new Enemy(new Vector2(600, 400), 50f)
+    new Enemy(new Vector2(400, 100), 50f, ShotFactory.CreateLaserShot),
+    new Enemy(new Vector2(200, 300), 50f, ShotFactory.CreateLaserShot),
+    new Enemy(new Vector2(600, 400), 50f, ShotFactory.CreateLaserShot)
 };
+
+var shots = new List<IShot>();
 
 while (!Raylib.WindowShouldClose())
 {
     // Movement input
     Vector2 move = Vector2.Zero;
     if (Raylib.IsKeyDown(KeyboardKey.Right)) move.X += 5;
-    if (Raylib.IsKeyDown(KeyboardKey.Left))  move.X -= 5;
-    if (Raylib.IsKeyDown(KeyboardKey.Up))    move.Y -= 5;
-    if (Raylib.IsKeyDown(KeyboardKey.Down))  move.Y += 5;
+    if (Raylib.IsKeyDown(KeyboardKey.Left)) move.X -= 5;
+    if (Raylib.IsKeyDown(KeyboardKey.Up)) move.Y -= 5;
+    if (Raylib.IsKeyDown(KeyboardKey.Down)) move.Y += 5;
     if (Raylib.IsKeyDown(KeyboardKey.Escape)) break;
+    if (Raylib.IsKeyPressed(KeyboardKey.Space))
+    {
+        // Player shoots a shot in the current direction
+        var shot = player.Shoot(new Vector2(1, 0)); // Default direction, can be overridden
+        shots.Add(shot);
+    }
 
     player.Move(move);
 
@@ -44,6 +51,13 @@ while (!Raylib.WindowShouldClose())
     foreach (var enemy in enemies)
     {
         Raylib.DrawRectangle((int)enemy.Position.X, (int)enemy.Position.Y, 50, 50, Color.Green);
+    }
+
+    // Update and draw shots
+    foreach (var shot in shots)
+    {
+        shot.Update();
+        Raylib.DrawRectangle((int)shot.Position.X, (int)shot.Position.Y, 10, 5, Color.Yellow);
     }
 
     Raylib.EndDrawing();
