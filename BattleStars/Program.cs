d02cac7a7cef233d1b06ebbba9b8a5d87a88712b
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-08-05 13:22:13 +0200

    Added a few enemies just for show

--- BattleStars/Program.cs
+++ BattleStars/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 using BattleStars;
+using System.Collections.Generic;
 
 Raylib.InitWindow(800, 600, "BattleStars - Square Test");
 Raylib.SetTargetFPS(60);
@@ -10,6 +11,14 @@ var boundaryChecker = new BoundaryChecker(0, 800 - 50, 0, 600 - 50); // 50 is th
 
 // Create player at starting position
 var player = new Player(new Vector2(100, 100), 100f, boundaryChecker);
+
+// Create enemies at different positions
+var enemies = new List<Enemy>
+{
+    new Enemy(new Vector2(400, 100), 50f),
+    new Enemy(new Vector2(200, 300), 50f),
+    new Enemy(new Vector2(600, 400), 50f)
+};
 
 while (!Raylib.WindowShouldClose())
 {
@@ -27,7 +36,16 @@ while (!Raylib.WindowShouldClose())
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.Black);
     Raylib.DrawText("Arrow keys move the square", 10, 10, 20, Color.White);
+
+    // Draw player
     Raylib.DrawRectangle((int)player.Position.X, (int)player.Position.Y, 50, 50, Color.Red);
+
+    // Draw enemies
+    foreach (var enemy in enemies)
+    {
+        Raylib.DrawRectangle((int)enemy.Position.X, (int)enemy.Position.Y, 50, 50, Color.Green);
+    }
+
     Raylib.EndDrawing();
 }
 
