224f4514eee01c6d646773f9b1e78ecb66885a22
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-09-09 16:32:42 +0200

    Implemented a BasicDestructable and tests for it.

---
+++ BattleStars/Core/BasicDestructable.cs
@@ -0,0 +1,32 @@
+using BattleStars.Utility;
+
+namespace BattleStars.Core
+{
+    public class BasicDestructable : IDestructable
+    {
+        private float _health;
+
+        public BasicDestructable(float health)
+        {
+            FloatValidator.ThrowIfNaNOrInfinity(health, nameof(health));
+            FloatValidator.ThrowIfNegativeOrZero(health, nameof(health));
+
+            _health = health;
+        }
+
+        public float Health
+        {
+            get => _health;
+            set => _health = value < 0 ? 0 : value;
+        }
+
+        public bool IsDestroyed => Health <= 0;
+        public void TakeDamage(float amount)
+        {
+            FloatValidator.ThrowIfNaNOrInfinity(amount, nameof(amount));
+            FloatValidator.ThrowIfNegative(amount, nameof(amount));
+
+            Health -= amount;
+        }
+    }
+}
