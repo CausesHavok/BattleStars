e92311a35d00ea082e689c1f5de4c42d182e74dd
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-09-12 15:30:02 +0200

    refac: remove Entity, Player, and Enemy classes
    Removed Entity, Player, and Enemy classes along with their corresponding tests.
    Removed CollisionChecker.CheckEntityShotCollision and updated CollisionCheckerTest accordingly.
    
    This cleanup eliminates deprecated classes and simplifies future development.

--- BattleStars.Tests/CollisionCheckerTest.cs
+++ BattleStars.Tests/CollisionCheckerTest.cs
@@ -11,66 +11,6 @@ namespace BattleStars.Tests.Logic;
 public class CollisionCheckerTest
 {
     [Fact]
-    public void GivenNullEntity_WhenCheckEntityShotCollision_ThenThrowsArgumentNullException()
-    {
-        var shotMock = new Mock<IShot>().Object;
-        Action act = () => CollisionChecker.CheckEntityShotCollision(null!, shotMock);
-        act.Should().Throw<ArgumentNullException>();
-    }
-
-    [Fact]
-    public void GivenNullShot_WhenCheckEntityShotCollision_ThenThrowsArgumentNullException()
-    {
-        var entityMock = new Mock<Entity>(MockBehavior.Strict).Object;
-        Action act = () => CollisionChecker.CheckEntityShotCollision(entityMock, null!);
-        act.Should().Throw<ArgumentNullException>();
-    }
-
-    [Fact]
-    public void GivenEntityAndShot_WhenShapeContainsAdjustedPosition_ThenReturnsTrue()
-    {
-        var entityMock = new Mock<Entity>(MockBehavior.Strict);
-        var shapeMock = new Mock<IShape>();
-        var shotMock = new Mock<IShot>();
-
-        var entityPosition = new Vector2(5, 5);
-        var shotPosition = new Vector2(7, 8);
-        var adjustedPosition = shotPosition - entityPosition;
-
-        entityMock.Setup(e => e.Position).Returns(entityPosition);
-        entityMock.Setup(e => e.Shape).Returns(shapeMock.Object);
-        shotMock.Setup(s => s.Position).Returns(shotPosition);
-        shapeMock.Setup(s => s.Contains(adjustedPosition)).Returns(true);
-
-        var result = CollisionChecker.CheckEntityShotCollision(entityMock.Object, shotMock.Object);
-
-        result.Should().BeTrue();
-        shapeMock.Verify(s => s.Contains(adjustedPosition), Times.Once);
-    }
-
-    [Fact]
-    public void GivenEntityAndShot_WhenShapeDoesNotContainAdjustedPosition_ThenReturnsFalse()
-    {
-        var entityMock = new Mock<Entity>(MockBehavior.Strict);
-        var shapeMock = new Mock<IShape>();
-        var shotMock = new Mock<IShot>();
-
-        var entityPosition = new Vector2(1, 1);
-        var shotPosition = new Vector2(10, 10);
-        var adjustedPosition = shotPosition - entityPosition;
-
-        entityMock.Setup(e => e.Position).Returns(entityPosition);
-        entityMock.Setup(e => e.Shape).Returns(shapeMock.Object);
-        shotMock.Setup(s => s.Position).Returns(shotPosition);
-        shapeMock.Setup(s => s.Contains(adjustedPosition)).Returns(false);
-
-        var result = CollisionChecker.CheckEntityShotCollision(entityMock.Object, shotMock.Object);
-
-        result.Should().BeFalse();
-        shapeMock.Verify(s => s.Contains(adjustedPosition), Times.Once);
-    }
-
-    [Fact]
     public void GivenNullBattleStar_WhenCheckBattleStarShotCollision_ThenThrowsArgumentNullException()
     {
         var shotMock = new Mock<IShot>().Object;
--- BattleStars/Logic/CollisionChecker.cs
+++ BattleStars/Logic/CollisionChecker.cs
@@ -4,14 +4,6 @@ namespace BattleStars.Logic;
 
 public static class CollisionChecker
 {
-    public static bool CheckEntityShotCollision(Entity entity, IShot shot)
-    {
-        ArgumentNullException.ThrowIfNull(entity);
-        ArgumentNullException.ThrowIfNull(shot);
-        var adjustedPosition = shot.Position - entity.Position;
-        return entity.Shape.Contains(adjustedPosition);
-    }
-
     public static bool CheckBattleStarShotCollision(BattleStar battleStar, IShot shot)
     {
         ArgumentNullException.ThrowIfNull(battleStar);
