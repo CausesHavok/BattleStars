492eae77c859475cd67f027f154350fb64269edd
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-09-17 14:19:50 +0200

    test: add units tests for BoundaryChecker.
    BoundaryChecker has been refactored to include more guards on input parameters.
    BoundaryCheckerTest has been created, which validates the behavoir of Boundary checker
    
    This ensure centralized uniform validation of input/output. It also ensure that futher changes are less likely to break the existing functionality

--- BattleStars/Logic/BoundaryChecker.cs
+++ BattleStars/Logic/BoundaryChecker.cs
@@ -1,10 +1,21 @@
+using BattleStars.Utility;
 namespace BattleStars.Logic;
+
 public class BoundaryChecker : IBoundaryChecker
 {
     private readonly float minX, maxX, minY, maxY;
 
     public BoundaryChecker(float minX, float maxX, float minY, float maxY)
     {
+        FloatValidator.ThrowIfNaNOrInfinity(minX, nameof(minX));
+        FloatValidator.ThrowIfNaNOrInfinity(maxX, nameof(maxX));
+        FloatValidator.ThrowIfNaNOrInfinity(minY, nameof(minY));
+        FloatValidator.ThrowIfNaNOrInfinity(maxY, nameof(maxY));
+        if (minX >= maxX)
+            throw new ArgumentException("minX must be less than maxX.");
+        if (minY >= maxY)
+            throw new ArgumentException("minY must be less than maxY.");
+
         this.minX = minX;
         this.maxX = maxX;
         this.minY = minY;
@@ -13,21 +24,25 @@ public class BoundaryChecker : IBoundaryChecker
 
     public bool IsOutsideXBounds(float x)
     {
+        FloatValidator.ThrowIfNaNOrInfinity(x, nameof(x));
         return x < minX || x > maxX;
     }
 
     public float XDistanceToBoundary(float x)
     {
+        FloatValidator.ThrowIfNaNOrInfinity(x, nameof(x));
         return Math.Min(Math.Abs(x - minX), Math.Abs(x - maxX));
     }
 
     public bool IsOutsideYBounds(float y)
     {
+        FloatValidator.ThrowIfNaNOrInfinity(y, nameof(y));
         return y < minY || y > maxY;
     }
 
     public float YDistanceToBoundary(float y)
     {
+        FloatValidator.ThrowIfNaNOrInfinity(y, nameof(y));
         return Math.Min(Math.Abs(y - minY), Math.Abs(y - maxY));
     }
 }
