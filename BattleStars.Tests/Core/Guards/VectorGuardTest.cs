f4db0777132d2c045a95d9a5765aa43270695c41
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-10-19 17:05:46 +0200

    test: add test cases covering paramName inferance in VectorGuardTest

--- BattleStars.Tests/Core/Guards/VectorGuardTest.cs
+++ BattleStars.Tests/Core/Guards/VectorGuardTest.cs
@@ -6,8 +6,29 @@ namespace BattleStars.Tests.Core.Guards;
 
 public class VectorGuardTest
 {
+    #region NaN
     [Fact]
-    public void GivenVectorWithNaNComponent_WhenThrowIfNaN_ThenThrowsArgumentException()
+    public void GivenNullName_WhenRequireNotNaN_ThenThrowsArgumentException()
+    {
+        var vector = new Vector2(float.NaN, 1f);
+        Action act = () => VectorGuard.RequireNotNaN(vector, null!);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("<value>.X cannot be NaN.*")
+            .WithParameterName("<value>.X");
+    }
+
+    [Fact]
+    public void GivenNoName_WhenRequireNotNaN_ThenThrowsArgumentException()
+    {
+        var vector = new Vector2(float.NaN, 1f);
+        Action act = () => VectorGuard.RequireNotNaN(vector);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("vector.X cannot be NaN.*")
+            .WithParameterName("vector.X");
+    }
+
+    [Fact]
+    public void GivenName_WhenRequireNotNaN_ThenThrowsArgumentException()
     {
         var vector = new Vector2(float.NaN, 1f);
         Action act = () => VectorGuard.RequireNotNaN(vector, "testVector");
@@ -16,8 +37,31 @@ public class VectorGuardTest
             .WithParameterName("testVector.X");
     }
 
+    #endregion
+
+    #region Finite
     [Fact]
-    public void GivenVectorWithInfinityComponent_WhenThrowIfInfinity_ThenThrowsArgumentException()
+    public void GivenNullName_WhenRequireFinite_ThenThrowsArgumentException()
+    {
+        var vector = new Vector2(1f, float.PositiveInfinity);
+        Action act = () => VectorGuard.RequireFinite(vector, null!);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("<value>.Y must be finite.*")
+            .WithParameterName("<value>.Y");
+    }
+
+    [Fact]
+    public void GivenNoName_WhenRequireFinite_ThenThrowsArgumentException()
+    {
+        var vector = new Vector2(1f, float.PositiveInfinity);
+        Action act = () => VectorGuard.RequireFinite(vector);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("vector.Y must be finite.*")
+            .WithParameterName("vector.Y");
+    }
+
+    [Fact]
+    public void GivenName_WhenRequireFinite_ThenThrowsArgumentException()
     {
         var vector = new Vector2(1f, float.PositiveInfinity);
         Action act = () => VectorGuard.RequireFinite(vector, "testVector");
@@ -26,8 +70,31 @@ public class VectorGuardTest
             .WithParameterName("testVector.Y");
     }
 
+    #endregion
+
+    #region NonZero
     [Fact]
-    public void GivenZeroVector_WhenThrowIfZero_ThenThrowsArgumentOutOfRangeException()
+    public void GivenNullName_WhenRequireNonZero_ThenThrowsArgumentOutOfRangeException()
+    {
+        var vector = Vector2.Zero;
+        Action act = () => VectorGuard.RequireNonZero(vector, null!);
+        act.Should().Throw<ArgumentOutOfRangeException>()
+            .WithMessage("<value> cannot be a zero vector.*")
+            .WithParameterName("<value>");
+    }
+
+    [Fact]
+    public void GivenNoName_WhenRequireNonZero_ThenThrowsArgumentOutOfRangeException()
+    {
+        var vector = Vector2.Zero;
+        Action act = () => VectorGuard.RequireNonZero(vector);
+        act.Should().Throw<ArgumentOutOfRangeException>()
+            .WithMessage("vector cannot be a zero vector.*")
+            .WithParameterName("vector");
+    }
+
+    [Fact]
+    public void GivenName_WhenRequireNonZero_ThenThrowsArgumentOutOfRangeException()
     {
         var vector = Vector2.Zero;
         Action act = () => VectorGuard.RequireNonZero(vector, "testVector");
@@ -36,8 +103,32 @@ public class VectorGuardTest
             .WithParameterName("testVector");
     }
 
+    #endregion
+
+    #region Normalized
+
     [Fact]
-    public void GivenNonNormalizedVector_WhenThrowIfNotNormalized_ThenThrowsArgumentException()
+    public void GivenNullName_WhenRequireNormalized_ThenThrowsArgumentException()
+    {
+        var vector = new Vector2(2, 0);
+        Action act = () => VectorGuard.RequireNormalized(vector, null!);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("<value> must be a normalized vector.*")
+            .WithParameterName("<value>");
+    }
+
+    [Fact]
+    public void GivenNoName_WhenRequireNormalized_ThenThrowsArgumentException()
+    {
+        var vector = new Vector2(2, 0);
+        Action act = () => VectorGuard.RequireNormalized(vector);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("vector must be a normalized vector.*")
+            .WithParameterName("vector");
+    }
+
+    [Fact]
+    public void GivenName_WhenRequireNormalized_ThenThrowsArgumentException()
     {
         var vector = new Vector2(2, 0);
         Action act = () => VectorGuard.RequireNormalized(vector, "testVector");
@@ -46,23 +137,41 @@ public class VectorGuardTest
             .WithParameterName("testVector");
     }
 
+    #endregion
+
+    #region Valid
+
     [Fact]
-    public void GivenNormalizedVector_WhenThrowIfNotNormalized_ThenDoesNotThrow()
+    public void GivenNullName_WhenRequireValid_ThenThrowsArgumentException()
     {
-        var vector = Vector2.Normalize(new Vector2(1, 1));
-        vector = Vector2.Normalize(vector); // Ensure it's normalized
-        Action act = () => VectorGuard.RequireNormalized(vector, "testVector");
-        act.Should().NotThrow();
+        var vector = new Vector2(float.NaN, float.PositiveInfinity);
+        Action act = () => VectorGuard.RequireValid(vector, null!);
+        act.Should().Throw<ArgumentException>("<value>.X cannot be NaN.*")
+            .WithParameterName("<value>.X");
     }
 
     [Fact]
-    public void GivenVectorWithNaNOrInfinity_WhenThrowIfNaNOrInfinity_ThenThrowsArgumentException()
+    public void GivenNoName_WhenRequireValid_ThenThrowsArgumentException()
+    {
+        var vector = new Vector2(float.NaN, float.PositiveInfinity);
+        Action act = () => VectorGuard.RequireValid(vector);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("vector.X cannot be NaN.*")
+            .WithParameterName("vector.X");
+    }
+
+    [Fact]
+    public void GivenName_WhenRequireValid_ThenThrowsArgumentException()
     {
         var vector = new Vector2(float.NaN, float.PositiveInfinity);
         Action act = () => VectorGuard.RequireValid(vector, "testVector");
-        act.Should().Throw<ArgumentException>("testVector.X cannot be NaN.*")
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("testVector.X cannot be NaN.*")
             .WithParameterName("testVector.X");
     }
+    #endregion
+
+    #region All Validators
 
     [Fact]
     public void GivenValidVector_WhenAllValidatorsCalled_ThenDoesNotThrow()
@@ -79,4 +188,5 @@ public class VectorGuardTest
 
         act.Should().NotThrow();
     }
+    #endregion
 }
