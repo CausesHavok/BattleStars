6f884ac2eddde3b97f980872a677c57d38d843c2
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-10-22 12:11:11 +0200

    refac: remove guards from Shotfactory

--- BattleStars/Infrastructure/Factories/ShotFactory.cs
+++ BattleStars/Infrastructure/Factories/ShotFactory.cs
@@ -1,24 +1,12 @@
 using BattleStars.Domain.Entities;
 using BattleStars.Domain.Interfaces;
 using BattleStars.Domain.ValueObjects;
-using BattleStars.Core.Guards;
 namespace BattleStars.Infrastructure.Factories;
 
 public static class ShotFactory
 {
-    private static void ValidateShotParameters(float speed, float damage)
-    {
-        Guard.RequireValid(speed, nameof(speed));
-        Guard.RequireValid(damage, nameof(damage));
-        Guard.RequireNonNegative(speed, nameof(speed));
-        Guard.RequireNonNegative(damage, nameof(damage));
-    }
-
-    public static IShot CustomShot(PositionalVector2 position, DirectionalVector2 direction, float speed, float damage)
-    {
-        ValidateShotParameters(speed, damage);
-        return new Shot(position, direction, speed, damage);
-    }
+    public static IShot CustomShot(PositionalVector2 position, DirectionalVector2 direction, float speed, float damage) 
+        => new Shot(position, direction, speed, damage);
 
     public static IShot CreateScatterShot(PositionalVector2 position, DirectionalVector2 direction)
         => CustomShot(position, direction, speed: 3f, damage: 3f);
