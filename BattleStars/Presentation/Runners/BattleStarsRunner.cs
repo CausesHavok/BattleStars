b5c754650a8142dde351fe823a7428a1bbf58a72
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-10-24 12:39:08 +0200

    refac: remove magic numbers etc. from BattleStarsRunner

--- BattleStars/Presentation/Runners/BattleStarsRunner.cs
+++ BattleStars/Presentation/Runners/BattleStarsRunner.cs
@@ -10,11 +10,15 @@ public class BattleStarsRunner(IFrameRenderer frameRenderer, IWindowConfiguratio
     private readonly IFrameRenderer _frameRenderer = Guard.NotNull(frameRenderer, nameof(frameRenderer));
     private readonly IWindowConfiguration _windowConfiguration = Guard.NotNull(windowConfiguration, nameof(windowConfiguration));
     private readonly IGameBootstrapper _gameBootstrapper = Guard.NotNull(gameBootstrapper, nameof(gameBootstrapper));
+    private const string Title = "BattleStars";
+    private const int TargetFps = 60;
+    private const int WindowWidth = 800;
+    private const int WindowHeight = 600;
 
     public void Run()
     {
-        _windowConfiguration.InitWindow(800, 600, "BattleStars - Square Test");
-        _windowConfiguration.SetTargetFPS(60);
+        _windowConfiguration.InitWindow(WindowWidth, WindowHeight, Title);
+        _windowConfiguration.SetTargetFPS(TargetFps);
 
         var gameController = _gameBootstrapper.Initialize().GameController;
         var shouldContinue = true;
