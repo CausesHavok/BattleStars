6e6bf52b8828f96117225e4bd2c42f089ab91d3d
Magnus <32954465+CausesHavok@users.noreply.github.com>
2025-10-24 11:50:33 +0200

    refac: extract ralib methods from program and add them to RaylibGraphocicsAdapter.

--- BattleStars/Program.cs
+++ BattleStars/Program.cs
@@ -1,13 +1,13 @@
-﻿using Raylib_cs;
-using BattleStars.Infrastructure.Startup;
+﻿using BattleStars.Infrastructure.Startup;
 using BattleStars.Infrastructure.Factories;
 using BattleStars.Presentation.Renderers;
+using BattleStars.Infrastructure.Adapters;
 
 int windowWidth  = 800;
 int windowHeight = 600;
-
-Raylib.InitWindow(windowWidth, windowHeight, "BattleStars - Square Test");
-Raylib.SetTargetFPS(60);
+var raylibAdapter = new RaylibGraphicsAdapter();
+raylibAdapter.InitWindow(windowWidth, windowHeight, "BattleStars - Square Test");
+raylibAdapter.SetTargetFPS(60);
 
 var drawer = SceneFactory.CreateShapeDrawer();
 var bootstrapper = new GameBootstrapper(windowWidth, windowHeight, drawer);
@@ -16,10 +16,10 @@ var gameController = bootstrapper.Initialize().GameController;
 var shouldContinue = true;
 var frameRenderer = new FrameRenderer();
 
-while (!Raylib.WindowShouldClose())
+while (!raylibAdapter.WindowShouldClose())
 {
     if (shouldContinue) shouldContinue = gameController.RunFrame();
     frameRenderer.RenderFrame(gameController.GetFrameSnapshot());
 }
 
-Raylib.CloseWindow();
+raylibAdapter.CloseWindow();
